22c2e8d58a5fe36d8be2a78c76092489e544057e
Tom Spencer <fiznool@gmail.com>
2016-11-18 10:42:09 +0000

    Create Feed List page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { FeedListPage } from '../pages/feed-list/feed-list';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    FeedListPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +14,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    FeedListPage
   ],
   providers: []
 })
